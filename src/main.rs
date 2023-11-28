6c591d1bcaf7bbcd5655ee66d5fb4b00a0054015
KUNYUAN XIAO <kunyuan.xiao@trumpf.com>
2023-11-28 14:19:03 +0800

    code clean

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@
 use std::net::TcpStream;
 use std::env;
 
-use std::collections::HashMap;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let args_as_str: Vec<&str> = args.iter().map(|s| s.as_ref()).collect();    
@@ -10,8 +9,7 @@ fn main() {
     if args_as_str.len() > 1
     {
         addr = args_as_str[1];
-    }
-    else{
+    }else{
         println!("Please input the ip and port e.g. 127.0.0.1:80");
         return;
     }
@@ -19,12 +17,6 @@ fn main() {
 }
 fn check(addr: &str)  {
     let stream = TcpStream::connect(addr);
-    // stream.write(&[1])?;
-    // let mut buf = [0; 128];
-    // match stream.read(&mut buf) {
-    //     Ok(_) => println!("Port is open!"),
-    //     Err(_) => println!("Port is closed!"),
-    // }
     match stream {
         Ok(_) => println!("Port is open"),
         Err(_) => println!("Port is closed"),
